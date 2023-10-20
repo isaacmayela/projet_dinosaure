07ddc22fcac6563ae0db2ac5443eeff7f104dd15
isaacmayela <isaacmayela27@gmail.com>
2023-10-20 18:09:36 +0200

    feat: i've start the project

--- index.js
+++ index.js
@@ -1,7 +1,55 @@
 const getJumpButton = document.getElementById("jump");
+const getRichard = document.getElementById("richard");
+const getBall = document.getElementById("ball");
 
-function jump() {
-    getJumpButton.style.top = "30%"
+let move = 0;
+function incrementor() {
+    move += 10;
+    // console.log(move);
 }
 
-getJumpButton.addEventListener("click",jump)
+function moveBall() {
+    setInterval(incrementor,1000);
+    console.log(move);
+    getBall.style.right = `${move}%`;
+    // incrementor += 10;
+}
+
+
+
+function ball_progression() {
+    let compteur = 0
+
+    function names() {
+
+        if (compteur  === 100){
+            compteur = 0;
+        }
+
+        compteur += 10;
+
+        getBall.style.right = `${compteur}%`;
+
+    }
+
+    setInterval(names, 90)
+
+}
+
+
+
+
+
+
+function down() {
+    getRichard.style.top = "65%";
+}
+
+function jump() {
+    getRichard.style.top = "35%";
+    setTimeout(down,300);
+}
+
+ball_progression();
+getJumpButton.addEventListener("click",jump);
+
